7335a3a4fd920d4ec5a186d3834ab30911e5e6e3
Agustin Lopez <aalopezba@gmail.com>
2026-03-15 02:38:46 -0300

    feat: add user model and schema

---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+class User(Base):
+    __tablename__:User = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+
+
---
+++ app/schemas/user.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    email: EmailStr = Field(..., description="User's email.")
+
+class UserCreate(UserBase):
+    password: str = Field(..., description="User's password.")
+
+class UserOut(UserBase):
+    id: int
+    is_active: bool
+    
+    class Config:
+        from_attributes: True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "Bearer"
+
+class TokenData(BaseModel):
+    email: str | None = None
