54fe13b17a1474616f9f2881f0903ad1d3971c00
Agustin Lopez <aalopezba@gmail.com>
2026-03-15 03:17:23 -0300

    feat: add oauth2 scheme and current user deps

---
+++ app/api/deps.py
@@ -0,0 +1,43 @@
+from re import NOFLAG
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+from app.core.config import settings
+from app.core.security import create_access_token
+from app.db.session import SessionLocal
+from app.models.user import User
+from app.schemas.user import TokenData
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        email: str | None = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
+
+    raise NotImplementedError("Not implemented yet.")
